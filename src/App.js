cb41dcbb243099132db1fd74d725acabdaf7ea7c
saurabhlm10 <saurabhlm10@gmail.com>
2023-06-07 11:10:14 +0530

    minor bug fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('https://socketio-server-env-2.eba-3p37vvyn.ap-south-1.elasticbeanstalk.com/');
+const socket = io('http://socketio-server-env-2.eba-3p37vvyn.ap-south-1.elasticbeanstalk.com/');
 
 function App() {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  
 
   useEffect(() => {
     socket.on('chat message', (msg) => {
